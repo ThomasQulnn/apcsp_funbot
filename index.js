d7f5ccd2d81e32cba82845d56444fa5aa4e7947b
ThomasQulnn <113370289+ThomasQulnn@users.noreply.github.com>
2022-09-26 11:22:09 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -48,3 +48,19 @@ client.on('interactionCreate', async interaction => {
 });
 
 client.login(token);
+
+
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
